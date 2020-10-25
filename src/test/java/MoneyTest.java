6f244ba20c80b691065aeb1c2bdfc7e48fa8d4b2
JINHUI KIM <xzdfor@naver.com>
2020-10-25 14:45:58 +0900

    [TDD] 09. Times We're Livin' In

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -27,4 +27,10 @@ public class MoneyTest {
         assertEquals(Money.franc(15), five.times(3));
     }
 
+    @Test
+    public void testCurrency() {
+        assertEquals("USD",Money.dollar(5).currency());
+        assertEquals("CHF", Money.franc(5).currency());
+    }
+
 }
